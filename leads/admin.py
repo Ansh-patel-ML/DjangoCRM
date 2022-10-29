f59b970db436ff428dec8097861bf31ae6f70e61
anshpatel <patelansh883@gmail.com>
2022-10-29 22:34:08 +0530

    added superUser

--- leads/admin.py
+++ leads/admin.py
@@ -1,1 +1,8 @@
+from curses.ascii import US
 from django.contrib import admin
+
+from .models import Lead, User, Agent
+
+admin.site.register(Lead)
+admin.site.register(User)
+admin.site.register(Agent)
--- leads/models.py
+++ leads/models.py
@@ -1,3 +1,4 @@
+from re import S
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -9,9 +10,15 @@ class User(AbstractUser):
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+    def __str__(self) -> str:
+        return self.user.first_name
+
 
 class Lead(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
     agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
